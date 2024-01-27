a96b360f3b86151e234a8f2fd4408fad55ff3ab4
karim sebbari <karim.sebbari@outlook.fr>
2024-01-27 18:19:25 +0100

    feature-slb10: create services interface

---
+++ EmployeeManagement.Services/IServices/IEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Domain.Models.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Services.IServices
+{
+    public interface IEmployeeService
+    {
+        void CreateEmployee(EmployeeCreateModel employee);
+        void UpdateEmployee(EmployeeUpdateModel employee);
+        void DeleteEmployee(EmployeeModel employee);
+        Task<List<EmployeeModel>> GetEmployees();
+        Task<EmployeeModel> GetEmployee(int id);
+    }
+}
